69ef8c68225a69aea328fe4aa16d9b9248e3934e
Karsenh <KarsenHansen@gmail.com>
2021-05-31 12:54:36 -0700

    Added useRef to prevent infinite updating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  // const [renderCount, setRenderCount] = useState(0);
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type='text' value={name} onChange={e => setName(e.target.value)} />
+      <h1>My name is {name} </h1>
+      <h2>I rendered {renderCount.current} times. </h2>
+    </>
   );
 }
 
